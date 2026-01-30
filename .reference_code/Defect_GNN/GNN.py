144c2b02b7d7e12c2bfb42534fee6c566af0d1d6
Benjamin Marler <146089920+bamarler@users.noreply.github.com>
2026-01-29 19:50:22 -0500

    formatted reference python code

--- .reference_code/Defect_GNN/GNN.py
+++ .reference_code/Defect_GNN/GNN.py
@@ -12,9 +12,20 @@ from models.models_together import CGNN, GAT, Transformer
 import utilities
 from dataset import Dataset
 
-class GNN():
-    def __init__(self, dataset_root, modelname, num_hidden_layers, num_hidden_channels, num_heads,
-                lr=0.1, weight_decay=5e-4, batchsz=128, max_epoch=100):
+
+class GNN:
+    def __init__(
+        self,
+        dataset_root,
+        modelname,
+        num_hidden_layers,
+        num_hidden_channels,
+        num_heads,
+        lr=0.1,
+        weight_decay=5e-4,
+        batchsz=128,
+        max_epoch=100,
+    ):
         torch_geometric.seed_everything(4)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -24,39 +35,47 @@ class GNN():
         data = dataset[0]
         num_node_features = data.num_node_features
         num_edge_features = data.num_edge_features
-        if hasattr(data, 'topo_features'):
+        if hasattr(data, "topo_features"):
             num_topo_features = data.topo_features.shape[1]
         else:
             num_topo_features = 0
 
-        if modelname == 'CGNN':
-            self.model = eval(modelname)(num_hidden_layers=num_hidden_layers, num_hidden_channels=num_hidden_channels,
-                                         num_node_features=num_node_features,
-                                         num_edge_features=num_edge_features,
-                                         num_topo_features=num_topo_features,
-                                         device=self.device)
-            self.name = f'{modelname}_{num_hidden_layers}_{num_hidden_channels}_{lr}_{weight_decay}_{batchsz}'
-        elif modelname == 'GAT' or modelname == 'Transformer':
-            assert num_hidden_channels % num_heads == 0, f'{modelname} network must have hidden channels ' \
-                                                         f'{num_hidden_channels} as multiples of head numbers {num_heads}'
-            self.model = eval(modelname)(num_hidden_layers=num_hidden_layers, num_hidden_channels=num_hidden_channels,
-                                         num_heads=num_heads,
-                                         num_node_features=num_node_features,
-                                         num_edge_features=num_edge_features,
-                                         num_topo_features=num_topo_features,
-                                         device=self.device)
-            self.name = f'{modelname}_{num_hidden_layers}_{num_hidden_channels}_{num_heads}_{lr}_{weight_decay}_{batchsz}'
+        if modelname == "CGNN":
+            self.model = eval(modelname)(
+                num_hidden_layers=num_hidden_layers,
+                num_hidden_channels=num_hidden_channels,
+                num_node_features=num_node_features,
+                num_edge_features=num_edge_features,
+                num_topo_features=num_topo_features,
+                device=self.device,
+            )
+            self.name = f"{modelname}_{num_hidden_layers}_{num_hidden_channels}_{lr}_{weight_decay}_{batchsz}"
+        elif modelname == "GAT" or modelname == "Transformer":
+            assert num_hidden_channels % num_heads == 0, (
+                f"{modelname} network must have hidden channels "
+                f"{num_hidden_channels} as multiples of head numbers {num_heads}"
+            )
+            self.model = eval(modelname)(
+                num_hidden_layers=num_hidden_layers,
+                num_hidden_channels=num_hidden_channels,
+                num_heads=num_heads,
+                num_node_features=num_node_features,
+                num_edge_features=num_edge_features,
+                num_topo_features=num_topo_features,
+                device=self.device,
+            )
+            self.name = f"{modelname}_{num_hidden_layers}_{num_hidden_channels}_{num_heads}_{lr}_{weight_decay}_{batchsz}"
         self.model.to(self.device)
 
         self.optimizer = Adam(self.model.parameters(), lr=lr, weight_decay=weight_decay)
         self.max_epoch = max_epoch
-        self.logger = utilities.get_logger(f'./logfiles/{self.name}')
+        self.logger = utilities.get_logger(f"./logfiles/{self.name}")
 
         self.train_loss = []
         self.val_loss = []
-        self.min_val_loss = float('inf')
+        self.min_val_loss = float("inf")
         self.early_schedule_step = 0
-        self.test_loss = float('inf')
+        self.test_loss = float("inf")
 
     def loadDataset(self, root):
         dataset = Dataset(root)
@@ -67,8 +86,10 @@ class GNN():
         total_size = len(dataset)
         train_size = int(total_size * train_ratio)
         val_size = int(total_size * val_ratio)
-        train_loader = DataLoader(dataset[: train_size], batch_size=batchsz)
-        val_loader = DataLoader(dataset[train_size : train_size + val_size], batch_size=batchsz)
+        train_loader = DataLoader(dataset[:train_size], batch_size=batchsz)
+        val_loader = DataLoader(
+            dataset[train_size : train_size + val_size], batch_size=batchsz
+        )
         test_loader = DataLoader(dataset[train_size + val_size :], batch_size=1)
         return [train_loader, val_loader, test_loader]
 
@@ -78,31 +99,31 @@ class GNN():
 
     def saveGNNResults(self):
         results = {
-            'train_loss': self.train_loss,
-            'val_loss': self.val_loss,
-            'test_loss': self.test_loss
+            "train_loss": self.train_loss,
+            "val_loss": self.val_loss,
+            "test_loss": self.test_loss,
         }
-        with open('./save/' + self.name + '.pkl', 'wb') as f:
+        with open("./save/" + self.name + ".pkl", "wb") as f:
             pkl.dump(results, f)
 
     def saveTrainedModel(self):
-        with open('./save/' + self.name + '_model.pkl', 'wb') as f:
+        with open("./save/" + self.name + "_model.pkl", "wb") as f:
             pkl.dump(self.model, f)
 
     def loadTrainedModel(self):
-        with open('./save/' + self.name + '_model.pkl', 'rb') as f:
+        with open("./save/" + self.name + "_model.pkl", "rb") as f:
             self.model = pkl.load(f)
 
     def plotLossValues(self):
         epoch = range(len(self.train_loss))
 
         fig = plt.figure()
-        plt.plot(epoch, self.train_loss, 'k', label='train')
-        plt.plot(epoch, self.val_loss, 'r', label='validation')
-        plt.xlabel('epoch')
-        plt.ylabel('loss')
+        plt.plot(epoch, self.train_loss, "k", label="train")
+        plt.plot(epoch, self.val_loss, "r", label="validation")
+        plt.xlabel("epoch")
+        plt.ylabel("loss")
         plt.legend()
-        plt.savefig('./save/' + self.name + '.png')
+        plt.savefig("./save/" + self.name + ".png")
         plt.close()
 
     def trainModel(self, loader):
@@ -132,8 +153,14 @@ class GNN():
         return eval_loss
 
     def run(self):
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'min', min_lr=1e-5)
-        train_loader, val_loader, test_loader = self.loaders[0], self.loaders[1], self.loaders[2]
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            self.optimizer, "min", min_lr=1e-5
+        )
+        train_loader, val_loader, test_loader = (
+            self.loaders[0],
+            self.loaders[1],
+            self.loaders[2],
+        )
 
         # self.logger.info('')
         for epoch in range(self.max_epoch):
@@ -141,7 +168,9 @@ class GNN():
             val_loss = self.evalModel(val_loader)
             self.train_loss.append(train_loss)
             self.val_loss.append(val_loss)
-            self.logger.info(f'epoch = {epoch}, training loss = {train_loss}, validation loss = {val_loss}')
+            self.logger.info(
+                f"epoch = {epoch}, training loss = {train_loss}, validation loss = {val_loss}"
+            )
             scheduler.step(train_loss)
 
             if val_loss < self.min_val_loss:
@@ -150,8 +179,10 @@ class GNN():
                 self.saveTrainedModel()
             else:
                 self.early_schedule_step += 1
-                self.logger.info(f'Early stopping step {self.early_schedule_step}, the current validation loss {val_loss}'
-                                 f' is larger than best value {self.min_val_loss}')
+                self.logger.info(
+                    f"Early stopping step {self.early_schedule_step}, the current validation loss {val_loss}"
+                    f" is larger than best value {self.min_val_loss}"
+                )
 
             # if self.early_schedule_step == 8:
             #     self.logger.info('Early stopped at epoch {}'.format(epoch))
@@ -159,50 +190,65 @@ class GNN():
 
         self.loadTrainedModel()
         self.test_loss = self.evalModel(test_loader)
-        self.logger.info('=' * 100)
-        self.logger.info(f'The testing loss is {self.test_loss}')
+        self.logger.info("=" * 100)
+        self.logger.info(f"The testing loss is {self.test_loss}")
         self.saveGNNResults()
         # self.plotLossValues()
 
-class GNN_optuna():
+
+class GNN_optuna:
     def __init__(self, dataset_root, num_trials=100):
         self.dataset_root = dataset_root
         self.num_trials = num_trials
 
     def save(self, study):
-        with open('./save/GNN_optuna.pkl', 'wb') as f:
+        with open("./save/GNN_optuna.pkl", "wb") as f:
             pkl.dump(study, f)
 
     def objective(self, trial):
-        modelname = trial.suggest_categorical('modelname', ['CGNN', 'GAT', 'Transformer'])
-        num_hidden_layers = trial.suggest_int('num_hidden_layers', 1, 8)
-        num_hidden_channels = trial.suggest_categorical('num_hidden_channels', [8, 16, 32, 64, 128, 256])
-        num_heads = trial.suggest_categorical('num_heads', [1, 2, 4, 8])
-        lr = trial.suggest_float('lr', 1e-3, 1e-1)
-        weight_decay = trial.suggest_float('weight_decay', 1e-4, 1e-1)
-        batchsz = trial.suggest_int('batchsz', 2, 48)
+        modelname = trial.suggest_categorical(
+            "modelname", ["CGNN", "GAT", "Transformer"]
+        )
+        num_hidden_layers = trial.suggest_int("num_hidden_layers", 1, 8)
+        num_hidden_channels = trial.suggest_categorical(
+            "num_hidden_channels", [8, 16, 32, 64, 128, 256]
+        )
+        num_heads = trial.suggest_categorical("num_heads", [1, 2, 4, 8])
+        lr = trial.suggest_float("lr", 1e-3, 1e-1)
+        weight_decay = trial.suggest_float("weight_decay", 1e-4, 1e-1)
+        batchsz = trial.suggest_int("batchsz", 2, 48)
 
-        GNN_trial = GNN(dataset_root=self.dataset_root, modelname=modelname, num_hidden_layers=num_hidden_layers, num_hidden_channels=num_hidden_channels,
-            num_heads=num_heads, lr=lr, weight_decay=weight_decay, batchsz=batchsz, max_epoch=100)
+        GNN_trial = GNN(
+            dataset_root=self.dataset_root,
+            modelname=modelname,
+            num_hidden_layers=num_hidden_layers,
+            num_hidden_channels=num_hidden_channels,
+            num_heads=num_heads,
+            lr=lr,
+            weight_decay=weight_decay,
+            batchsz=batchsz,
+            max_epoch=100,
+        )
         GNN_trial.run()
         return GNN_trial.test_loss
 
     def run(self):
-        study = optuna.create_study(direction='minimize')
+        study = optuna.create_study(direction="minimize")
         study.optimize(self.objective, n_trials=self.num_trials)
 
         print("Best trial:")
         trial = study.best_trial
         print("  Value: ", trial.value)
         print("  Params: ")
-        for key, value in trial.params.items(): print("{}: {}".format(key, value))
+        for key, value in trial.params.items():
+            print("{}: {}".format(key, value))
 
         self.save(study)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     torch_geometric.seed_everything(4)
-    dataset_root = 'datasets/'
+    dataset_root = "datasets/"
 
     # GNN = GNN(dataset_root=dataset_root,
     #           modelname='CGNN',
@@ -218,10 +264,3 @@ if __name__ == '__main__':
 
     GNN_optuna = GNN_optuna(dataset_root=dataset_root, num_trials=200)
     GNN_optuna.run()
-
-
-
-
-
-
-
--- .reference_code/Defect_GNN/utilities.py
+++ .reference_code/Defect_GNN/utilities.py
@@ -10,6 +10,7 @@ from pymatgen.core.periodic_table import Element
 
 from torch_geometric.data import Data
 
+
 def getPymatgenElementProperties(number):
     element = Element.from_Z(number)
     properties = []
@@ -22,23 +23,36 @@ def getPymatgenElementProperties(number):
     # properties += list(element.data['Atomic orbitals'].values())[-6:-1]
     return properties
 
+
 def calculateEdgeAttributes(dist, r_cutoff, dr):
     if dr == 0:
         return dist
     else:
         rgrid = np.arange(0, r_cutoff, dr)
         sigma = r_cutoff / 3
-        attr = np.exp(-0.5 * (rgrid - dist)**2 /sigma**2) / np.sqrt(2 * np.pi) / sigma
+        attr = (
+            np.exp(-0.5 * (rgrid - dist) ** 2 / sigma**2) / np.sqrt(2 * np.pi) / sigma
+        )
         return attr
 
-def structureToGraph(structure, E=None, atom_dict=None, topo_features=None, dist_to_vac=None, r_cutoff=10, dr=0.1, max_neighbors=20):
+
+def structureToGraph(
+    structure,
+    E=None,
+    atom_dict=None,
+    topo_features=None,
+    dist_to_vac=None,
+    r_cutoff=10,
+    dr=0.1,
+    max_neighbors=20,
+):
     neighbors = structure.get_all_neighbors(r_cutoff, include_index=True)
     all_nbrs = [sorted(nbrs, key=lambda x: x.nn_distance) for nbrs in neighbors]
     neighbors_idx, neighbors_dist = [], []
     max_neighbors = min(max_neighbors, len(all_nbrs[0]))
     for nbr in all_nbrs:
-        neighbors_idx.append(list(map(lambda x: x.index, nbr[ : max_neighbors])))
-        neighbors_dist.append(list(map(lambda x: x.nn_distance, nbr[ : max_neighbors])))
+        neighbors_idx.append(list(map(lambda x: x.index, nbr[:max_neighbors])))
+        neighbors_dist.append(list(map(lambda x: x.nn_distance, nbr[:max_neighbors])))
 
     x = []
     edge_index = []
@@ -48,7 +62,9 @@ def structureToGraph(structure, E=None, atom_dict=None, topo_features=None, dist
         x.append(elemi)
         for j in range(len(neighbors_idx[i])):
             edge_index.append([i, neighbors_idx[i][j]])
-            edge_attr.append(calculateEdgeAttributes(neighbors_dist[i][j], r_cutoff=r_cutoff, dr=dr))
+            edge_attr.append(
+                calculateEdgeAttributes(neighbors_dist[i][j], r_cutoff=r_cutoff, dr=dr)
+            )
 
     x = torch.tensor(np.array(x), dtype=torch.float)
     edge_index = torch.tensor(np.array(edge_index), dtype=torch.long).t().contiguous()
@@ -61,26 +77,33 @@ def structureToGraph(structure, E=None, atom_dict=None, topo_features=None, dist
         dist_to_vac = torch.tensor(np.array(dist_to_vac), dtype=torch.float).t()
 
     if E is None:
-        y = torch.tensor(float('inf'))
+        y = torch.tensor(float("inf"))
     else:
         y = torch.tensor(E, dtype=torch.float)
 
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y,
-                topo_features=topo_features, dist_to_vac=dist_to_vac,
-                )
+    data = Data(
+        x=x,
+        edge_index=edge_index,
+        edge_attr=edge_attr,
+        y=y,
+        topo_features=topo_features,
+        dist_to_vac=dist_to_vac,
+    )
     return data
+
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    filename = name + '.log'
-    fh = logging.FileHandler(filename, mode='w')
+    filename = name + ".log"
+    fh = logging.FileHandler(filename, mode="w")
     logger.addHandler(fh)
     logger.setLevel(logging.INFO)
     return logger
 
+
 class CPU_Unpickler(pkl.Unpickler):
     def find_class(self, module, name):
-        if module == 'torch.storage' and name == '_load_from_bytes':
-            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        if module == "torch.storage" and name == "_load_from_bytes":
+            return lambda b: torch.load(io.BytesIO(b), map_location="cpu")
         else:
             return super().find_class(module, name)
